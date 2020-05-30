f4c0b23930e661c3222b478bf58b940e5d34d2a0
feng gu <fgu30@gatech.edu>
2020-05-30 14:06:35 -0400

    main structure

---
+++ src/main/java/GeeWay/MainVerticle.java
@@ -0,0 +1,13 @@
+package GeeWay;
+
+import GeeWay.asyncresponse.ProductBackend;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class MainVerticle extends AbstractVerticle {
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        vertx.deployVerticle(ProductBackend.class.getName());
+        vertx.deployVerticle(HttpVerticle.class.getName());
+    }
+}
