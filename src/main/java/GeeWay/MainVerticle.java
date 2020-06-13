1398a09cb9cef2f75a23315154dfde3736550805
feng gu <fgu30@gatech.edu>
2020-06-13 12:13:01 -0400

    new

--- src/main/java/GeeWay/MainVerticle.java
+++ src/main/java/GeeWay/MainVerticle.java
@@ -1,13 +1,17 @@
 package GeeWay;
 
-import GeeWay.asyncresponse.ProductBackend;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle(new MainVerticle());
+    }
+
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
-        vertx.deployVerticle(ProductBackend.class.getName());
-        vertx.deployVerticle(HttpVerticle.class.getName());
+
     }
 }
