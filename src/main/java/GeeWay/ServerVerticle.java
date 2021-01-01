d8ecc615a2d3b23c0fa0efaa118034986d7f62fb
feng gu <fgu30@gatech.edu>
2021-01-01 13:23:06 -0500

    fix bug

--- src/main/java/GeeWay/ServerVerticle.java
+++ src/main/java/GeeWay/ServerVerticle.java
@@ -1,2 +1,43 @@
-package GeeWay;public class ServerVerticle {
+package GeeWay;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+
+public class ServerVerticle extends AbstractVerticle {
+    @Override
+    public void start() throws Exception {
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+
+        router.get("/hello").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            HttpServerRequest request = routingContext.request();
+            response.end("hello world");
+        });
+
+        router.post("/hello")
+                .handler(BodyHandler.create())
+                .handler(routingContext -> {
+                    JsonObject json = routingContext.getBodyAsJson();
+                    routingContext.response().end(json.getString("name"));
+                });
+
+        router.errorHandler(500, rc -> {
+            rc.failure().printStackTrace();
+            rc.response().setStatusCode(500).end("我错了");
+        });
+
+        server.requestHandler(router).listen(8080, event -> {
+            if (event.succeeded()) {
+                System.out.println("server at port : 8080");
+            }
+        });
+
+    }
 }
